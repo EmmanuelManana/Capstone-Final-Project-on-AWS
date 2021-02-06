617626ea0572a58add9a3b0f4dad675b841431db
EmmanuelManana <emanana@student.wethinkcode.co.za>
2021-02-06 14:48:27 +0200

    user-service-tested

--- lambda_function.py
+++ lambda_function.py
@@ -19,7 +19,7 @@ def lambda_handler(event, context):
     write_to_file('/tmp/dog.jpg', encodedImage) 
     
     try:
-        #read as bytes
+        #read bytes
         imgFile = open('/tmp/dog.jpg', 'rb')
         imgBytes = imgFile.read()
         imgFile.clode()
@@ -27,7 +27,15 @@ def lambda_handler(event, context):
         print('Could not read the file')
         
     imgobj = {'Bytes': imgBytes}
+    response_labels = client.detect_labels(Image = imgobj)
+    
+    print(response_labels)
     
     return {
-        
+        'body' : json.dumps(response_labels)
     }
+
+# response_labels, model was able to detect the image and identy its contents.
+# {'Labels': [{'Name': 'Manga', 'Confidence': 99.71334075927734, 'Instances': [], 'Parents': [{'Name': 'Comics'}, {'Name': 'Book'}]}, {'Name': 'Comics', 'Confidence': 99.71334075927734, 'Instances': [], 'Parents': [{'Name': 'Book'}]}, {'Name': 'Book', 'Confidence': 99.71334075927734, 'Instances': [], 'Parents': []}, {'Name': 'Person', 'Confidence': 78.16822814941406, 'Instances': [{'BoundingBox': {'Width': 0.5692188143730164, 'Height': 0.9165003895759583, 'Left': 0.3229805827140808, 'Top': 0.05902675911784172}, 'Confidence': 78.16822814941406}], 'Parents': []}, {'Name': 'Human', 'Confidence': 78.16822814941406, 'Instances': [], 'Parents': []}], 'LabelModelVersion': '2.0', 'ResponseMetadata': {'RequestId': '65ab097b-a990-4f32-b756-08dde75d51d9', 'HTTPStatusCode': 200, 'HTTPHeaders': {'content-type': 'application/x-amz-json-1.1', 'date': 'Thu, 04 Feb 2021 21:48:39 GMT', 'x-amzn-requestid': '65ab097b-a990-4f32-b756-08dde75d51d9', 'content-length': '622', 'connection': 'keep-alive'}, 'RetryAttempts': 0}}
+# END RequestId: bd06dd56-3a78-496c-92fe-2725f46bf2f3
+# REPORT RequestId: bd06dd56-3a78-496c-92fe-2725f46bf2f3	Duration: 1150.17 ms	Billed Duration: 1151 ms	Memory Size: 128 MB	Max Memory Used: 79 MB	
--- user-service/src/functions/createUser.js
+++ user-service/src/functions/createUser.js
@@ -1,38 +1,36 @@
 'use strict';
 const { ComputeOptimizer } = require('aws-sdk');
 const AWS = require('aws-sdk')
+const bycrpt = require('bcryptjs')
 
 module.exports.createUser = async (event, context) => {
-
-  const body = JSON.parse(event.body);
-  const username = body.username;
-  const password = body.password;
-
+  const salt = await bcryptjs.genSalt(10)
+  const body = JSON.parse(event.body)
+  const username = body.username
+  const password = body.password
   const newUser = {
     TableName: process.env.DYNAMODB_USER_TABLE,
     Item: {
       pk: username,
-      password: password
+      password: bycrpt.hashSync(password, salt)
     }
   }
-
   try {
-    const dynamodb = new AWS.DynamoDB.DocumentClient();
-    await dynamodb.put(newUser).promise();
-
+    const dynamodb = new AWS.DynamoDB.DocumentClient()
+    const registerUser = await dynamodb.put(newUser).promise()
     return {
       statusCode: 201,
-      headers:{
+      headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
         'Access-Control-Allow-Headers': 'Authorization'
       }
-    };
-
-  } catch (erro) {
-    console.error("User Creation erro Error")
-    console.log("new user", newUser);
-    return new Error("User Creation erro Error");
+    }
+  } catch(Error) {
+    console.log('Error create new User')
+    console.log('putError', Error)
+    console.log('newUser Payload', newUser)
+    return new Error('Error create new User')
   }
 
 };
