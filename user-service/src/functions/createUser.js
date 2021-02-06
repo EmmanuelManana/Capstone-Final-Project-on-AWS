5427eb1be38c846de3f6c6dc66d571aec58e5558
EmmanuelManana <emanana@student.wethinkcode.co.za>
2021-02-06 11:20:30 +0200

    user registratin

---
+++ user-service/src/functions/createUser.js
@@ -0,0 +1,38 @@
+'use strict';
+const { ComputeOptimizer } = require('aws-sdk');
+const AWS = require('aws-sdk')
+
+module.exports.createUser = async (event, context) => {
+
+  const body = JSON.parse(event.body);
+  const username = body.username;
+  const password = body.password;
+
+  const newUser = {
+    TableName: process.env.DYNAMODB_USER_TABLE,
+    Item: {
+      pk: username,
+      password: password
+    }
+  }
+
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const createUser = await dynamodb.put(newUser).promise();
+
+    return {
+      statusCode: 201,
+      headers:{
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Authorization'
+      }
+    };
+
+  } catch (erro) {
+    console.error("User Creation erro Error")
+    console.log("new user", newUser);
+    return new Error("User Creation erro Error");
+  }
+
+};
